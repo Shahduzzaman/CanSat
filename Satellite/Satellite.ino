ad7c30a8111f03a96efc43bfb130e3920d21ea4b
Shahduzzaman <mdshahduzzaman@gmail.com>
2024-11-07 12:51:12 +0600

    MPU code inserted

--- Satellite/Satellite.ino
+++ Satellite/Satellite.ino
@@ -17,7 +17,7 @@
 #define I2C_ADDRESS 0x77
 
 RF24 radio(7, 8); // CE, CSN pins
-const byte address[6] = "00001";
+const byte address[6] = "00169";
 
 DHT dht(DHTPIN, DHTTYPE);
 BMP180I2C bmp180(I2C_ADDRESS);
@@ -33,6 +33,49 @@ bool blinkState = false;
 float RateRoll, RatePitch, RateYaw;
 float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
 int RateCalibrationNumber;
+
+uint16_t dig_T1, dig_P1;
+int16_t  dig_T2, dig_T3, dig_P2, dig_P3, dig_P4, dig_P5;
+int16_t  dig_P6, dig_P7, dig_P8, dig_P9; 
+float AltitudeBarometer, AltitudeBarometerStartUp;
+//int RateCalibrationNumber;
+
+void barometer_signals(void){
+  Wire.beginTransmission(0x76);
+  Wire.write(0xF7);
+  Wire.endTransmission();
+  Wire.requestFrom(0x76,6);
+  uint32_t press_msb = Wire.read();
+  uint32_t press_lsb = Wire.read();
+  uint32_t press_xlsb = Wire.read();
+  uint32_t temp_msb = Wire.read();
+  uint32_t temp_lsb = Wire.read();
+  uint32_t temp_xlsb = Wire.read();
+  unsigned long int adc_P = (press_msb << 12) | (press_lsb << 4) | (press_xlsb >>4);
+  unsigned long int adc_T = (temp_msb << 12) | (temp_lsb << 4) | (temp_xlsb >>4);
+  signed long int var1, var2;
+  var1 = ((((adc_T >> 3) - ((signed long int )dig_T1 <<1)))* ((signed long int )dig_T2)) >> 11;
+  var2 = (((((adc_T >> 4) - ((signed long int )dig_T1)) * ((adc_T>>4) - ((signed long int )dig_T1)))>> 12) * ((signed long int )dig_T3)) >> 14;
+  signed long int t_fine = var1 + var2;
+  unsigned long int p;
+  var1 = (((signed long int )t_fine)>>1) - (signed long int )64000;
+  var2 = (((var1>>2) * (var1>>2)) >> 11) * ((signed long int )dig_P6);
+  var2 = var2 + ((var1*((signed long int )dig_P5)) <<1);
+  var2 = (var2>>2)+(((signed long int )dig_P4)<<16);
+  var1 = (((dig_P3 * (((var1>>2)*(var1>>2)) >> 13))>>3)+((((signed long int )dig_P2) * var1)>>1))>>18;
+  var1 = ((((32768+var1))*((signed long int )dig_P1)) >>15);
+  if (var1 == 0) { p=0;}    
+  p = (((unsigned long int )(((signed long int ) 1048576)-adc_P)-(var2>>12)))*3125;
+  if(p<0x80000000){ p = (p << 1) / ((unsigned long int ) var1);}
+  else { p = (p / (unsigned long int )var1) * 2;  }
+  var1 = (((signed long int )dig_P9) * ((signed long int ) (((p>>3) * (p>>3))>>13)))>>12;
+  var2 = (((signed long int )(p>>2)) * ((signed long int )dig_P8))>>13;
+  p = (unsigned long int)((signed long int )p + ((var1 + var2+ dig_P7) >> 4));
+  double pressure=(double)p/100;
+  AltitudeBarometer=44330*(1-pow(pressure
+     /1013.25, 1/5.255))*100;
+}
+
 void gyro_signals(void) {
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
@@ -70,6 +113,47 @@ void setup() {
   }
   bmp180.resetToDefaults();
   bmp180.setSamplingMode(BMP180MI::MODE_UHR);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x76);
+  Wire.write(0xF4);
+  Wire.write(0x57);
+  Wire.endTransmission();   
+  Wire.beginTransmission(0x76);
+  Wire.write(0xF5); 
+  Wire.write(0x14);
+  Wire.endTransmission();   
+  uint8_t data[24], i=0; 
+  Wire.beginTransmission(0x76);
+  Wire.write(0x88);
+  Wire.endTransmission();
+  Wire.requestFrom(0x76,24);      
+  while(Wire.available()){
+    data[i] = Wire.read();
+    i++;
+  }
+  dig_T1 = (data[1] << 8) | data[0]; 
+  dig_T2 = (data[3] << 8) | data[2];
+  dig_T3 = (data[5] << 8) | data[4];
+  dig_P1 = (data[7] << 8) | data[6]; 
+  dig_P2 = (data[9] << 8) | data[8];
+  dig_P3 = (data[11]<< 8) | data[10];
+  dig_P4 = (data[13]<< 8) | data[12];
+  dig_P5 = (data[15]<< 8) | data[14];
+  dig_P6 = (data[17]<< 8) | data[16];
+  dig_P7 = (data[19]<< 8) | data[18];
+  dig_P8 = (data[21]<< 8) | data[20];
+  dig_P9 = (data[23]<< 8) | data[22]; delay(250);
+  for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+    barometer_signals();
+    AltitudeBarometerStartUp+=AltitudeBarometer;
+    delay(1);
+  }
+  AltitudeBarometerStartUp/=2000;
 
   //mpu code
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -136,7 +220,7 @@ void loop() {
   {
     delay(100);
   } while (!bmp180.hasValue());
-  Pressure = bmp180.getPressure();
+  Pressure = bmp180.readPressure() / 100.0F;
   
   //mpu code
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
@@ -157,6 +241,9 @@ void loop() {
   RatePitch-=RateCalibrationPitch;
   RateYaw-=RateCalibrationYaw;
 
+  barometer_signals();
+  AltitudeBarometer-=AltitudeBarometerStartUp;
+  
   // Create a struct to hold the data
   struct {
     float humidity;
@@ -167,18 +254,21 @@ void loop() {
     float RateRoll;
     float RatePitch;
     float RateYaw;
-  } data = {Humidity, Temperature, Pressure, filteredAngleX, filteredAngleY, RateRoll, RatePitch, RateYaw};
+    float AltitudeBarometer;
+  } data = {Humidity, Temperature, Pressure, filteredAngleX, filteredAngleY, RateRoll, RatePitch, RateYaw, AltitudeBarometer};
 
   // Send the data
   radio.write(&data, sizeof(data));
 
   // Print to check from transmitter end, only for testing time.
-  Serial.print("Humidity: "); Serial.print(data.humidity);
-  Serial.print(" Temperature: "); Serial.print(data.temp); Serial.print(" °C");
-  Serial.print(" Pressure: "); Serial.print(data.pressure);
+  Serial.print("Humid.: "); Serial.print(data.humidity);
+  Serial.print(" Temp.: "); Serial.print(data.temp); Serial.print(" °C");
+  Serial.print(" Pres.: "); Serial.print(data.pressure);
   //Serial.print(" Roll: "); Serial.print(data.angleX);
   //Serial.print(" Pitch: "); Serial.println(data.angleY);
   Serial.print(" Roll rate [°/s]= "); Serial.print(data.RateRoll); 
   Serial.print(" Pitch Rate [°/s]= "); Serial.print(data.RatePitch);
-  Serial.print(" Yaw Rate [°/s]= "); Serial.println(data.RateYaw);
+  Serial.print(" Yaw Rate [°/s]= "); Serial.print(data.RateYaw);
+  Serial.print(" Altitude [cm]: "); Serial.println(data.AltitudeBarometer);
+  
 }
